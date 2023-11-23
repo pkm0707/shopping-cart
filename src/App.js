e5640f0bb7eb01742214e7038b004c50e4781d65
pkm0707 <mpraveenkumar@student.tce.edu>
2023-11-23 20:24:10 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 import * as React from 'react';
-import {Container,Toolbar,AppBar,Typography,IconButton,Badge,Button,Card,CardActions,CardContent,CardMedia} from '@mui/material';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'; 
+import Badge from 'react-bootstrap/Badge';
+import Rating from '@mui/material/Rating';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Iconbutton } from './iconbutton';
+import { useState } from 'react';
 function App() {
   return (
     <div className="App">
@@ -10,216 +19,176 @@ function App() {
   );
 }
 function Shopingcart(){
+  const[count,setCount]=useState(0)
   return(
     <div className='main'>
-      <div>
-        <AppBar position="static">
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="#"
-                sx={{
-                    mr: 2,
-                    display: { xs: 'none', md: 'flex' },
-                    fontFamily: 'monospace',
-                    fontWeight: 700,
-                    letterSpacing: '.3rem',
-                    color: 'inherit',
-                    textDecoration: 'none',
-                }}
-              >
-                <b>BOOTSTRAP</b>
-                <Button>Home</Button>
-                <Button>About</Button>
-                <Button></Button>
-              </Typography>
-              <IconButton aria-label="cart">
-                <Badge color="secondary" badgeContent={0} showZero>
-                  <ShoppingCartIcon />
-                </Badge>
-              </IconButton>
-            </Toolbar>
-          </Container>
-        </AppBar>
-      </div>
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#" style={{fontSize:"25px"}}>Start Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto" style={{fontSize:"20px"}}>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <NavDropdown title="Shop" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#all-products">All Products</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#all-products/popular-items">
+                  Popular Items
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#all-products/new-arrivals">New Arrivals</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Iconbutton/>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <div className='header'>
         <h1 style={{fontSize:"60px",marginBottom:"5px",color:"white"}}>Shop in style</h1>
         <p style={{fontSize:"26px",marginTop:"5px",color:"white",opacity:"0.3"}}>With this shop homepage template</p>
       </div>
       <div>
         <div className='card-flex'>
-          <Card sx={{ maxWidth: 300}}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://m.media-amazon.com/images/I/51qSnKMJ6RL._SX300_SY300_QL70_FMwebp_.jpg"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
+          <Card style={{ width: '300px',position:"relative"}}>
+            <Card.Img variant="top" src="https://m.media-amazon.com/images/I/61ZCngQmGDL._SX342_.jpg"/>
+            <Card.Body>
+              <Card.Title style={{textAlign:"center",fontSize:"22px"}}><b>Citizen Analog Black</b></Card.Title>
+              <Card.Text style={{textAlign:"center"}}>
+                <Rating name="half-rating-read" defaultValue={5} precision={0.5} readOnly />
+              </Card.Text>
+              <Card.Text style={{textAlign:"center",fontSize:"20px"}}>
+                <s>₹29,900</s> ₹26,900 
+              </Card.Text>
+              <div className='button-center'>
+                <Button variant="primary" onClick={()=>setCount(count+1)}>Add to cart</Button>
+                <Button variant="danger" onClick={()=>setCount(count-1)} style={{margin:"0px 0px 0px 5px"}}>Remove from cart</Button>
+              </div>
+            </Card.Body>
+            <Badge bg="primary" style={{width:"70px",position:"absolute",marginTop:"10px",marginLeft:"220px"}}>offer-sale</Badge>
           </Card>
-          <Card sx={{ maxWidth: 300}}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://m.media-amazon.com/images/I/51qSnKMJ6RL._SX300_SY300_QL70_FMwebp_.jpg"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
+          <Card style={{ width: '300px',position:"relative"}}>
+            <Card.Img variant="top" src="https://m.media-amazon.com/images/I/51qSnKMJ6RL._SX300_SY300_QL70_FMwebp_.jpg" />
+            <Card.Body>
+              <Card.Title style={{textAlign:"center",fontSize:"22px"}}><b>Marshall Stanmore II</b></Card.Title>
+              <Card.Text style={{textAlign:"center"}}>
+                <Rating name="half-rating-read" defaultValue={4} precision={0.5} readOnly />
+              </Card.Text>
+              <Card.Text style={{textAlign:"center",fontSize:"20px"}}>
+                <s>₹39,999</s> ₹34,999 
+              </Card.Text>
+              <div className='button-center'>
+                <Button variant="primary" onClick={()=>setCount(count+1)}>Add to cart</Button>
+                <Button variant="danger" onClick={()=>setCount(count-1)} style={{margin:"0px 0px 0px 5px"}}>Remove from cart</Button>
+              </div>
+            </Card.Body>
+            <Badge bg="primary" style={{width:"70px",position:"absolute",marginTop:"10px",marginLeft:"220px"}}>offer-sale</Badge>
           </Card>
-          <Card sx={{ maxWidth: 300}}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://m.media-amazon.com/images/I/51qSnKMJ6RL._SX300_SY300_QL70_FMwebp_.jpg"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
+          <Card style={{ width: '300px'}}>
+            <Card.Img variant="top" src="https://m.media-amazon.com/images/I/71d7rfSl0wL._SX679_.jpg"/>
+            <Card.Body>
+              <Card.Title style={{textAlign:"center",fontSize:"22px"}}><b>Apple iPhone 15</b></Card.Title>
+              <Card.Text style={{textAlign:"center"}}>
+                <Rating name="half-rating-read" defaultValue={5} precision={0.5} readOnly />
+              </Card.Text>
+              <Card.Text style={{textAlign:"center",fontSize:"20px"}}>
+                ₹79,900 
+              </Card.Text>
+              <div className='button-center'>
+                <Button variant="primary" onClick={()=>setCount(count+1)}>Add to cart</Button>
+                <Button variant="danger" onClick={()=>setCount(count-1)} style={{margin:"0px 0px 0px 5px"}}>Remove from cart</Button>
+              </div>
+            </Card.Body>
           </Card>
-          <Card sx={{ maxWidth: 300}}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://m.media-amazon.com/images/I/51qSnKMJ6RL._SX300_SY300_QL70_FMwebp_.jpg"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
+          <Card style={{ width: '300px',position:"relative"}}>
+            <Card.Img variant="top" src="https://m.media-amazon.com/images/I/71TPda7cwUL._SX679_.jpg"/>
+            <Card.Body>
+              <Card.Title style={{textAlign:"center",fontSize:"22px"}}><b>Apple MacBook Air M1C</b></Card.Title>
+              <Card.Text style={{textAlign:"center"}}>
+                <Rating name="half-rating-read" defaultValue={5} precision={0.5} readOnly />
+              </Card.Text>
+              <Card.Text style={{textAlign:"center",fontSize:"20px"}}>
+                <s>₹99,900</s> ₹84,990
+              </Card.Text>
+              <div className='button-center'>
+                <Button variant="primary" onClick={()=>setCount(count+1)}>Add to cart</Button>
+                <Button variant="danger" onClick={()=>setCount(count-1)} style={{margin:"0px 0px 0px 5px"}}>Remove from cart</Button>
+              </div>
+            </Card.Body>
+            <Badge bg="primary" style={{width:"70px",position:"absolute",marginTop:"10px",marginLeft:"220px"}}>offer-sale</Badge>
           </Card>
-          <Card sx={{ maxWidth: 300}}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://m.media-amazon.com/images/I/51qSnKMJ6RL._SX300_SY300_QL70_FMwebp_.jpg"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
+          <Card style={{ width: '300px',position:"relative"}}>
+            <Card.Img variant="top" src="https://m.media-amazon.com/images/I/31IhYwtLhPL._SX300_SY300_QL70_FMwebp_.jpg"/>
+            <Card.Body>
+              <Card.Title style={{textAlign:"center",fontSize:"22px"}}><b>Apple Watch SE 2nd-G</b></Card.Title>
+              <Card.Text style={{textAlign:"center"}}>
+                <Rating name="half-rating-read" defaultValue={5} precision={0.5} readOnly />
+              </Card.Text>
+              <Card.Text style={{textAlign:"center",fontSize:"20px"}}>
+                <s>₹32,900</s> ₹29,400 
+              </Card.Text>
+              <div className='button-center'>
+                <Button variant="primary" onClick={()=>setCount(count+1)}>Add to cart</Button>
+                <Button variant="danger" onClick={()=>setCount(count-1)} style={{margin:"0px 0px 0px 5px"}}>Remove from cart</Button>
+              </div>
+            </Card.Body>
+            <Badge bg="primary" style={{width:"70px",position:"absolute",marginTop:"10px",marginLeft:"220px"}}>offer-sale</Badge>
           </Card>
-          <Card sx={{ maxWidth: 300}}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://m.media-amazon.com/images/I/51qSnKMJ6RL._SX300_SY300_QL70_FMwebp_.jpg"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
+          <Card style={{ width: '300px',position:"relative"}}>
+            <Card.Img variant="top" src="https://m.media-amazon.com/images/I/41du-jmuX1L._SX300_SY300_QL70_FMwebp_.jpg"/>
+            <Card.Body>
+              <Card.Title style={{textAlign:"center",fontSize:"22px"}}><b>Zebronics NBC 5S</b></Card.Title>
+              <Card.Text style={{textAlign:"center"}}>
+                <Rating name="half-rating-read" defaultValue={3} precision={0.5} readOnly />
+              </Card.Text>
+              <Card.Text style={{textAlign:"center",fontSize:"20px"}}>
+                <s>₹81,999</s> ₹54,990 
+              </Card.Text>
+              <div className='button-center'>
+                <Button variant="primary" onClick={()=>setCount(count+1)}>Add to cart</Button>
+                <Button variant="danger" onClick={()=>setCount(count-1)} style={{margin:"0px 0px 0px 5px"}}>Remove from cart</Button>
+              </div>
+            </Card.Body>
+            <Badge bg="primary" style={{width:"70px",position:"absolute",marginTop:"10px",marginLeft:"220px"}}>offer-sale</Badge>
           </Card>
-          <Card sx={{ maxWidth: 300}}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://m.media-amazon.com/images/I/51qSnKMJ6RL._SX300_SY300_QL70_FMwebp_.jpg"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
+          <Card style={{ width: '300px'}}>
+            <Card.Img variant="top" src="https://m.media-amazon.com/images/I/71nH3-koucL._SX679_.jpg"/>
+            <Card.Body>
+              <Card.Title style={{textAlign:"center",fontSize:"22px"}}><b>Disel Analog Black Dial</b></Card.Title>
+              <Card.Text style={{textAlign:"center"}}>
+                <Rating name="half-rating-read" defaultValue={4} precision={0.5} readOnly />
+              </Card.Text>
+              <Card.Text style={{textAlign:"center",fontSize:"20px"}}>
+                ₹33,995
+              </Card.Text>
+              <div className='button-center'>
+                <Button variant="primary" onClick={()=>setCount(count+1)}>Add to cart</Button>
+                <Button variant="danger" onClick={()=>setCount(count-1)} style={{margin:"0px 0px 0px 5px"}}>Remove from cart</Button>
+              </div>
+            </Card.Body>
           </Card>
-          <Card sx={{ maxWidth: 300}}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://m.media-amazon.com/images/I/51qSnKMJ6RL._SX300_SY300_QL70_FMwebp_.jpg"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
+          <Card style={{ width: '300px',position:"relative"}}>
+            <Card.Img variant="top" src="https://m.media-amazon.com/images/I/61e183Wpy2L._SX342_.jpg"/>
+            <Card.Body>
+              <Card.Title style={{textAlign:"center",fontSize:"22px"}}><b>GUESS Phoenix</b></Card.Title>
+              <Card.Text style={{textAlign:"center"}}>
+                <Rating name="half-rating-read" defaultValue={2} precision={0.5} readOnly />
+              </Card.Text>
+              <Card.Text style={{textAlign:"center",fontSize:"20px"}}>
+                <s>₹14,999</s> ₹10,999 
+              </Card.Text>
+              <div className='button-center'>
+                <Button variant="primary" onClick={()=>setCount(count+1)}>Add to cart</Button>
+                <Button variant="danger" onClick={()=>setCount(count-1)} style={{margin:"0px 0px 0px 5px"}}>Remove from cart</Button>
+              </div>
+            </Card.Body>
+            <Badge bg="primary" style={{width:"70px",position:"absolute",marginTop:"10px",marginLeft:"220px"}}>offer-sale</Badge>
           </Card>
         </div>
+      </div>
+      <div className='footer'>
+        <p style={{fontSize:"20px",color:"white"}}>Copyright © Your Website 2023</p>
       </div>
     </div>
   );
